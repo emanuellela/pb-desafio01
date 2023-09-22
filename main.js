2ef4ea52ae6e8889c76562f7beb680744b45908c
emanuellela <emanuellela@hotmail.com>
2023-09-22 02:41:05 -0300

    ajustes

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
 const hamburgerButton = document.querySelector('.hamburger-button');
-
 hamburgerButton.addEventListener('click', () => {
   hamburgerButton.classList.toggle('active');
-});
-
-document.addEventListener("DOMContentLoaded", function() {
-  const dropdownButton = document.getElementById("dropdown-button");
-
-  dropdownButton.addEventListener("click", function() {
-      dropdownButton.focus();
-  });
 });
