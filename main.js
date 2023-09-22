fcf1d91f60c984a99de63ea4b4477ee58326ce30
emanuellela <emanuellela@hotmail.com>
2023-09-21 23:59:47 -0300

    add dropdown button

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ const hamburgerButton = document.querySelector('.hamburger-button');
 hamburgerButton.addEventListener('click', () => {
   hamburgerButton.classList.toggle('active');
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const dropdownButton = document.getElementById("dropdown-button");
+
+  dropdownButton.addEventListener("click", function() {
+      dropdownButton.focus();
+  });
+});
